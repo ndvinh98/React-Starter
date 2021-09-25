55f5c89f369a082d2682504f5258b42008869487
Nguyennd507 <nguyennd507@gmail.com>
2021-09-25 09:41:31 +0700

    update

--- src/modules/home/modules/user-management/pages/user-management.list.tsx
+++ src/modules/home/modules/user-management/pages/user-management.list.tsx
@@ -1,13 +1,35 @@
 import React, {useEffect} from 'react';
-import * as UI from '@chakra-ui/react';
-import {AiOutlineSearch} from 'react-icons/ai';
-import {IFormControl} from '@components/FormGenerate/FormControl';
+
+import TableGenerate from '@components/TableGenerate';
 import FormGenerate from '@components/FormGenerate';
+import Pagination from '@components/Pagination';
+import {IUserManagement} from '@types';
+import Select from '@components/Select';
+
 import {useRouter, useFilter, useGetList} from '@utils/hooks';
 import {useRouterController} from '@modules/router';
-import {IUserManagement} from '@types';
 import {useMedia} from '@utils/hooks';
+
 import {isEmpty} from 'lodash';
+
+import * as UI from '@chakra-ui/react';
+import {AiOutlineSearch} from 'react-icons/ai';
+import {HiDotsHorizontal} from 'react-icons/hi';
+
+export const USRTYPE_STRING = {
+  PARTNERADMIN: 'Owner',
+  ADMIN: 'Admin',
+  USER: 'User',
+};
+
+export const STATUS_STRING = {
+  1: 'Has site access',
+  0: 'No site access',
+};
+export const ACTIVE_STRING = {
+  1: 'Actived',
+  0: 'Decatived',
+};
 
 function userTable() {
   const {push} = useRouter();
@@ -24,8 +46,7 @@ function userTable() {
     setFilter,
     setLimit,
   } = useFilter({page: 1, limit: 10});
-  const {data, getList, loading} =
-    useGetList<IUserManagement>('/user-management');
+  const {data, getList, loading} = useGetList<IUserManagement>('/users');
 
   const isHiden = false;
 
@@ -61,11 +82,11 @@ function userTable() {
   return (
     <UI.VStack py={6} px={8} spacing={4} width="full">
       <UI.Text fontSize="2xl" fontWeight="semibold" w="full">
-        Partner Management
+        User Management
       </UI.Text>
       <UI.Box width="full">
         <FormGenerate
-          gap={isBase ? 6 : 2}
+          gap={isBase ? 4 : 2}
           onChangeValue={handleFilterData}
           fields={[
             {
@@ -135,25 +156,239 @@ function userTable() {
               colSpan: isBase ? 3 : 12,
               DecorComponent: () => {
                 return (
-                  <UI.HStack
-                    position={{md: 'absolute', lg: 'static'}}
-                    top={'-166px'}
-                    w={'full'}
-                    justifyContent={'flex-end'}>
-                    <UI.Button
-                      onClick={() => push('/user-management/add-new')}
-                      size={'md'}>
-                      Add new user
-                    </UI.Button>
-                  </UI.HStack>
+                  <UI.VStack>
+                    <UI.HStack
+                      position={{md: 'absolute', lg: 'static'}}
+                      top={'-166px'}
+                      w={'full'}
+                      justifyContent={'space-between'}>
+                      <UI.Button
+                        minW={110}
+                        onClick={() => push('/user-management/add-new')}
+                        size={'md'}>
+                        Add new user
+                      </UI.Button>
+                      <UI.HStack
+                        position={{md: 'absolute', lg: 'static'}}
+                        top={'-166px'}
+                        w={'full'}
+                        justifyContent={'flex-end'}>
+                        <UI.Text> View Item</UI.Text>
+                        <Select
+                          isClearable={false}
+                          size="sm"
+                          name="limit"
+                          defaultValue={{
+                            label: '10',
+                            value: '10',
+                          }}
+                          options={[
+                            {
+                              label: '10',
+                              value: '10',
+                            },
+                            {
+                              label: '20',
+                              value: '20',
+                            },
+                            {
+                              label: 'all',
+                              value: 'all',
+                            },
+                          ]}
+                        />
+                      </UI.HStack>
+                    </UI.HStack>
+                  </UI.VStack>
                 );
               },
             },
           ]}
         />
+        {isBase ? (
+          <TableGenerate
+            //   onClickRow={(row) => push(path + `/${data?.id}`)}
+            isLoading={loading}
+            currentPage={data?.page}
+            totalpage={data?.totalPages}
+            data={data?.records || []}
+            onChangePage={setPage}
+            Footer={
+              <UI.Tr>
+                <UI.Td fontWeight="bold" colSpan={1000}>
+                  {data?.total} User
+                </UI.Td>
+              </UI.Tr>
+            }
+            columns={[
+              {
+                Header: 'User',
+                id: 'user',
+                accessor: (row) => (
+                  <UI.HStack>
+                    <UI.Avatar
+                      sx={{
+                        img: {
+                          objectFit: 'contain',
+                        },
+                      }}
+                      bg={
+                        row?.partnerUserProfiles?.[0]?.avatarMediaDestination
+                          ? 'white'
+                          : undefined
+                      }
+                      src={
+                        row?.partnerUserProfiles?.[0]?.avatarMediaDestination
+                      }
+                      name={row?.firstName + ' ' + row?.lastName}
+                      size={'sm'}
+                    />
+                    <UI.Text>
+                      {row?.firstName} {row?.lastName}
+                    </UI.Text>
+                  </UI.HStack>
+                ),
+              },
+              {
+                Header: 'Email Address',
+                id: 'emailAddress',
+                accessor: (row) => <UI.Text>{row?.email}</UI.Text>,
+              },
+
+              {
+                Header: 'Status',
+                id: 'status',
+                accessor: (row) => {
+                  return (
+                    <UI.Text>{STATUS_STRING?.[row?.hasAccess] || ''}</UI.Text>
+                  );
+                },
+              },
+              {
+                Header: 'Role',
+                id: 'role',
+                accessor: (row) => {
+                  return (
+                    <UI.Text>{USRTYPE_STRING?.[row?.userType] || ''}</UI.Text>
+                  );
+                },
+              },
+
+              {
+                Header: () => <UI.Center>Action</UI.Center>,
+                id: 'action',
+                accessor: (row) => (
+                  <ActionColum
+                    refresh={() =>
+                      getList({
+                        page: 1,
+                        limit: 10,
+                      })
+                    }
+                    row={row}
+                  />
+                ),
+              },
+            ]}
+          />
+        ) : (
+          <UI.Box>
+            {loading ? (
+              <UI.Center minH="300px">
+                <UI.Spinner size="lg" color="ste.red" />
+              </UI.Center>
+            ) : isEmpty(data?.records) ? (
+              <UI.Center>No data</UI.Center>
+            ) : (
+              <UI.VStack bg="white" spacing={3} p={3}>
+                <UI.HStack
+                  alignItems="center"
+                  justifyContent="space-between"
+                  w="full">
+                  <UI.Text w="200px" fontWeight="semibold">
+                    {data?.total} User
+                  </UI.Text>
+                  <Pagination
+                    currentPage={data?.page}
+                    totalpage={data?.totalPages}
+                    onChangePage={setPage}
+                    size={'sm'}
+                    justifyContent="flex-end"
+                  />
+                </UI.HStack>
+              </UI.VStack>
+            )}
+          </UI.Box>
+        )}
       </UI.Box>
     </UI.VStack>
   );
 }
+
+export const ActionColum = (props: any) => {
+  const {isOpen, onOpen, onClose} = UI.useDisclosure();
+  // const {openModal} = useModalStore();
+  const {row, refresh} = props;
+  return (
+    <UI.Center>
+      <UI.Menu onClose={onClose} isOpen={isOpen}>
+        <UI.MenuButton
+          px={4}
+          py={2}
+          onClick={(e) => {
+            e.stopPropagation();
+            onOpen();
+          }}>
+          <HiDotsHorizontal color={'#9097A9'} size={20} />
+        </UI.MenuButton>
+        <UI.MenuList minWidth={'150px'}>
+          <UI.MenuItem
+            isDisabled={row?.isActive === 1}
+            // onClick={(e) => {
+            //   e.stopPropagation();
+            //   openModal('action', {
+            //     title: 'Activate Access',
+            //     type: 'Activate',
+            //     id: row?.id,
+            //     cb: () => refresh(),
+            //   });
+            // }}
+          >
+            Activate Access
+          </UI.MenuItem>
+          <UI.MenuItem
+            isDisabled={row?.isActive === 0}
+            // onClick={(e) => {
+            //   e.stopPropagation();
+            //   openModal('action', {
+            //     title: 'Deactivate Access',
+            //     type: 'Deactivate',
+            //     id: row?.id,
+            //     cb: () => refresh(),
+            //   });
+            // }}
+          >
+            Deactivate Access
+          </UI.MenuItem>
+          <UI.MenuItem
+            isDisabled={row?.userType === 'PARTNERADMIN'}
+            // onClick={(e) => {
+            //   e.stopPropagation();
+            //   openModal('action', {
+            //     title: 'Change role',
+            //     type: 'change-role',
+            //     id: row?.id,
+            //     currentUserType: row?.userType,
+            //     cb: () => refresh(),
+            //   });
+            // }}
+          >
+            Change Role
+          </UI.MenuItem>
+        </UI.MenuList>
+      </UI.Menu>
+    </UI.Center>
+  );
+};
 
 export default userTable;
