8cbf29f3a7c0615ee151b76d1d2965196b005117
ndvinh98 <kaiznguyen98@gmail.com>
2021-10-04 17:29:19 +0700

    [ADD] add feedback form list view

--- src/modules/home/modules/feedback/pages/feedback-list.tsx
+++ src/modules/home/modules/feedback/pages/feedback-list.tsx
@@ -1,7 +1,233 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {isEmpty} from 'lodash';
+import * as UI from '@chakra-ui/react';
+import FormGenerate from '@components/FormGenerate';
+import {
+  AiOutlineSearch,
+  AiOutlineAppstore,
+  AiOutlineBars,
+} from 'react-icons/ai';
+import Pagination from '@components/Pagination';
+import {useMedia} from '@utils/hooks';
+import {useRouter, useFilter, useGetList} from '@utils/hooks';
+import groupBy from 'lodash/groupBy';
+import {format} from 'date-fns';
+import _ from 'lodash';
+const FeedbackContent = ({data}) => {
+  //const {openModal} = useModalStore();
+  // const {getItem, item} = useGetItem('partnerUserNotifications/read/');
+  // const readNoti = () => {
+  //   getItem({}, {path: data?.id});
+  // };
+
+  // React.useEffect(() => {
+  //   if (item) getTotalUnread();
+  // }, [item]);
+
+  // const [isRead, setIsRead] = useState(!!+data?.isRead);
+
+  // const groupedMessages = groupBy(data, function(n) {
+  //   return format(new Date(n.createdAt), 'dd MMM yyyy');
+  // });
+
+  const groupedMessages = _(data)
+    .groupBy((item) => format(new Date(item.createdAt), 'dd MMM yyyy'))
+    .sortBy((group) => data.indexOf(group[0]))
+    .value();
+
+  return (
+    <UI.Box>
+      {groupedMessages.map((group, gr_index) => {
+        const section = format(new Date(group[0].createdAt), 'dd MMM yyyy')
+        return (
+          <UI.Box key={gr_index} pt={4} mb={4}>
+            <UI.Text pb={2}> {section} </UI.Text>
+            { group.map((item, item_index) => {
+              return (
+                <UI.Flex
+                  flexDirection={{md: 'column', lg: 'row'}}
+                  key = {item_index}
+                  // onClick={() => {
+                  //   if (isRead) return;
+                  //   readNoti();
+                  //   setIsRead(true);
+                  // }}
+                  cursor="pointer"
+                  w="full"
+                  shadow="sm"
+                  p={3}
+                  //bg={isRead ? 'transparent' : 'white'}
+                  bg={'white'}
+                  justifyContent="space-between"
+                  //border={isRead ? ' 1px solid #E0E0E0' : 'none'}
+                  mb={2}
+                >
+                  <UI.HStack>
+                    <UI.Center
+                      w={{md: '40px', lg: '70px'}}
+                      p={{md: 1, lg: 2}}
+                      bg="white"
+                      shadow="md">
+                      <UI.Image
+                        w={{md: '30px', lg: '50px'}}
+                        h={{md: '30px', lg: '50px'}}
+                        src="/images/feedback-icon.svg"
+                        alt="feedback.png"
+                      />
+                    </UI.Center>
+                    <UI.Text fontSize={{md: 'md', lg: 'lg'}}>
+                      {item?.feedbackMessage}
+                    </UI.Text>
+                  </UI.HStack>
+
+                  <UI.HStack pr={4} pt={{md: 4, lg: 0}} justifyContent={'center'}>
+                    <UI.Button
+                      bgColor={'#E9E9E9'}
+                      color={'#54565A'}
+                      size={'sm'}
+                      _hover={{bgColor: '#28C76F', color: 'white'}}
+                      colorScheme="#EEFCEA">
+                      View
+                    </UI.Button>
+                  </UI.HStack>
+                </UI.Flex>
+              )})}
+          </UI.Box>
+        )
+      })}
+    </UI.Box>
+  );
+};
 
 function FeedbackList() {
-  return <div>this is FeedbackList</div>;
+  const {data, loading, getList} = useGetList('/partnerUserFeedbacks');
+  const {page, limit, setLimit, textSearch, setTextSearch, filter, setFilter} =
+    useFilter({
+      page: 1,
+      limit: 10,
+    });
+
+  useEffect(() => {
+    getList({
+      page,
+      limit,
+      sort: JSON.stringify({createdAt: -1}),
+      textSearch: textSearch
+        ? JSON.stringify([{feedbackMessage: textSearch}])
+        : undefined,
+      filter: isEmpty(filter) ? undefined : JSON.stringify([filter]),
+    });
+  }, [limit, page, textSearch, filter]);
+
+  const {isBase, isAllMobile} = useMedia();
+
+  const handleOnChange = ({textSearch, isRead}) => {
+    if (textSearch !== undefined) setTextSearch(textSearch as string);
+
+    if (isRead !== undefined) {
+      setFilter((state) => ({...state, isRead}));
+    }
+    if (isRead < 0) setFilter((state) => ({...state, isRead: undefined}));
+  };
+
+  return (
+    <UI.Box p={8} fontSize={'20px'}>
+      <UI.Text>Feedback Form</UI.Text>
+      <UI.Box minH="79vh" mt={4}>
+        <FormGenerate
+          gap={{md: 2, lg: 6}}
+          onChangeValue={handleOnChange}
+          fields={[
+            {
+              type: 'input-group',
+              name: 'textSearch',
+              colSpan: isBase ? 3 : 12,
+              size: 'md',
+              placeholder: 'Search...',
+              leftIcon: <AiOutlineSearch size={20} />,
+            },
+            {
+              type: 'select',
+              colSpan: isBase ? 3 : 12,
+              size: 'md',
+              name: 'isRead',
+              isClearable: false,
+              defaultValue: {
+                label: 'All feedback',
+                value: '-1',
+              },
+              options: [
+                {
+                  label: 'All feedback',
+                  value: '-1',
+                },
+                {
+                  label: 'Read',
+                  value: '1',
+                },
+                {
+                  label: 'Unread',
+                  value: '0',
+                },
+              ],
+            },
+          ]}
+        />
+
+        <UI.Box position="relative">
+          {loading && (
+            <UI.VStack
+              top="0px"
+              left="50%"
+              bg="rgba(196, 196, 196, 0.3)"
+              transform="translateX(-50%)"
+              position="absolute"
+              alignItems="center"
+              justifyContent="center"
+              zIndex="9"
+              w="full"
+              h="full"
+              minH="60px">
+              <UI.Center>
+                <UI.Spinner color="ste.red" size="lg" />
+              </UI.Center>
+            </UI.VStack>
+          )}
+          {isEmpty(data?.records) ? (
+            <UI.Center opacity={0.7} w={'full'} h={'300px'}>
+              <UI.VStack>
+                <UI.Image src={'https://i.imgur.com/nvdeBu8.png'} />
+                <UI.Text pt={2} color={'gray'} fontSize={'lg'}>
+                  No results found :((
+                </UI.Text>
+              </UI.VStack>
+            </UI.Center>
+          ) : (
+            <FeedbackContent data={data?.records} />
+          )}
+        </UI.Box>
+        {data?.totalPages > 1 && !isAllMobile && (
+          <UI.VStack mt={5}>
+            <UI.Button isLoading={loading} onClick={() => setLimit(limit + 10)}>
+              Load more
+            </UI.Button>
+          </UI.VStack>
+        )}
+      </UI.Box>
+      {isAllMobile && (
+        <Pagination
+          justifyContent={{md: 'center', lg: 'flex-end'}}
+          mt={6}
+          size={isAllMobile ? 'sm' : 'md'}
+          currentPage={data?.page || 1}
+          totalpage={data?.totalPages || 1}
+          onChangePage={(number) => {
+            getList({page: number, limit: 10, textSearch: undefined});
+          }}
+        />
+      )}
+    </UI.Box>
+  );
 }
 
 export default FeedbackList;
