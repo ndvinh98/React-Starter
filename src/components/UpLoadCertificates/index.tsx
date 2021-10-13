0650a0afa01e8a90c9dbd67e3cd5576223646a5d
phucpham <phuc.pxp@gmail.com>
2021-10-14 00:22:58 +0700

    new code

--- src/components/UpLoadCertificates/index.tsx
+++ src/components/UpLoadCertificates/index.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import * as UI from '@chakra-ui/react';
+import {useGetItem} from '@utils/hooks';
 
-import UploadFile from '@components/FormGenerate/fields/UploadFile';
-function UpLoadCertificates() {
+import UploadSingerFile from '@components/UploadSingerFile';
+import {uploadFile} from '@services';
+
+function UpLoadCertificates({partnerUserId}) {
+  const [file, setFile] = useState();
+  const {loading, getItem, data} = useGetItem<{url: string}>(
+    '/partnerUserCertificates/uploadFileUrl',
+  );
+
+  const toast = UI.useToast();
+
+  useEffect(() => {
+    if (data) {
+      uploadFile(data?.url, file).then(() =>
+        toast({
+          title: 'Successfully!',
+          status: 'success',
+          duration: 2000,
+          position: 'top-right',
+          isClosable: true,
+        }),
+      );
+    }
+  }, [data]);
+
   return (
-    <UI.VStack w="full">
-      <UploadFile />
+    <UI.VStack alignItems="flex-start" px={5} py={3} bg="white" w="full">
+      <UI.Text fontSize="18px">Upload Certificates</UI.Text>
+      <UploadSingerFile
+        isLoading={loading}
+        name="certificate"
+        onChangeValue={({certificate}) => {
+          if (certificate?.[0]) {
+            setFile(certificate?.[0]);
+            getItem({
+              partnerUserId,
+              certificateName: certificate?.[0].name,
+              name: certificate?.[0].name,
+              type: certificate?.[0].type,
+            });
+          }
+        }}
+      />
     </UI.VStack>
   );
 }
---
+++ src/components/UploadSingerFile/index.tsx
@@ -0,0 +1,94 @@
+import React, {useEffect, useState, useRef, memo} from 'react';
+import {isEmpty} from 'lodash';
+import {BiCloudUpload} from 'react-icons/bi';
+import * as UI from '@chakra-ui/react';
+import {FOLDER} from '@assets/base64/folder';
+
+interface IUploadFilesPorps {
+  label?: string;
+  className?: string;
+  name?: string;
+  onChangeValue?: (data: any) => void;
+  defaultValue?: any[];
+  isDisabled?: boolean;
+  isHiddenLabel?: boolean;
+  isLoading?: boolean;
+}
+
+function UploadFiles(props: IUploadFilesPorps) {
+  const {
+    name = 'upload-file',
+    onChangeValue,
+    defaultValue,
+    isDisabled,
+    isLoading,
+  } = props;
+
+  const [files, setFiles] = useState<any[]>(defaultValue);
+  const isChanged = useRef(false);
+
+  useEffect(() => {
+    if (onChangeValue && isChanged.current) onChangeValue({[name]: files});
+  }, [files]);
+
+  const onFileChange = (event: any) => {
+    isChanged.current = true;
+    if (!isEmpty(event.target.files)) setFiles([...event.target.files]);
+  };
+
+  return (
+    <>
+      <UI.Center
+        bg="#F8FBFF"
+        px={7}
+        py={5}
+        borderWidth="2px"
+        borderRadius="md"
+        borderStyle="dashed"
+        w="full">
+        <UI.Box
+          onDrop={
+            isDisabled
+              ? undefined
+              : (files) => {
+                  isChanged.current = true;
+                  if (!isEmpty(files)) setFiles(files as any);
+                }
+          }>
+          <UI.VStack>
+            <UI.Box>
+              <img width="40px" src={FOLDER} alt="folder-icon" />
+            </UI.Box>
+            <UI.Text>Drag & Drop your files here</UI.Text>
+            <UI.Text> Or</UI.Text>
+            <UI.Button
+              isLoading={isLoading}
+              cursor="pointer"
+              py={2}
+              px={3}
+              borderRadius={'md'}
+              as={'label'}
+              htmlFor={name}>
+              <UI.Center>
+                <BiCloudUpload size={20} />{' '}
+                <UI.Text ml={1}>Upload file</UI.Text>
+              </UI.Center>
+            </UI.Button>
+          </UI.VStack>
+
+          <UI.VisuallyHidden height={0}>
+            <input
+              onChange={onFileChange}
+              className="upload-input"
+              type="file"
+              disabled={isDisabled}
+              id={name}
+            />
+          </UI.VisuallyHidden>
+        </UI.Box>
+      </UI.Center>
+    </>
+  );
+}
+
+export default memo(UploadFiles);
