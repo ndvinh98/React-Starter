038a224f40a8ef98dcb0742b320414311ee2a568
ndvinh98 <kaiznguyen98@gmail.com>
2021-10-14 20:54:44 +0700

    done

--- src/modules/home/modules/content-management/modules/Modules/pages/add-new.tsx
+++ src/modules/home/modules/content-management/modules/Modules/pages/add-new.tsx
@@ -1,7 +1,251 @@
-import React from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
+import * as UI from '@chakra-ui/react';
+import {BsArrowLeft} from 'react-icons/bs';
+import {useRouter, useGetItem, usePost, useGetList} from '@utils/hooks';
+import {IoMdCloseCircle} from 'react-icons/io';
+import {IProduct, ICategorie, IGrouping} from '@types';
+import UploadThumb from '@components/UploadThumb';
+import {uploadFile} from '@services';
+
+const STOCK = [
+  'https://i.imgur.com/Q04dMOc.png',
+  'https://i.imgur.com/2NpnErN.png',
+  'https://i.imgur.com/wSX6CPl.png',
+  'https://i.imgur.com/Y3lAWKG.png',
+  'https://i.imgur.com/1okhbVx.png',
+];
 
 function AddNew() {
-  return <div>This is add new</div>;
+  const {getItem: getAllMenu, data: menuData} = useGetItem('applications/menu');
+  const [name, setName] = useState('');
+  const [application, setApplication] = useState('');
+  const [category, setCategory] = useState('');
+  const [grouping, setGroup] = useState('');
+  const [product, setProduct] = useState('');
+  useEffect(() => {
+    getAllMenu();
+  }, []);
+
+  const {getList: getListCategories, data: categoriesData} =
+    useGetList<ICategorie>('categories');
+  const {getList: getListGroupings, data: groupingsData} =
+    useGetList<IGrouping>('groupings');
+  const {getList: getListProduct, data: productsData} =
+    useGetList<IProduct>('products');
+
+  useEffect(() => {
+    if (application)
+      getListCategories({limit: 9999,filter: JSON.stringify([{application: application}])});
+    if (category)
+      getListGroupings({limit: 9999,filter: JSON.stringify([{category: category}])});
+    if (grouping)
+      getListProduct({limit: 9999,filter: JSON.stringify([{grouping: grouping}])});
+  }, [grouping, category, application]);
+
+  const {push} = useRouter();
+  const toast = UI.useToast();
+
+  const handleChange = (event) => setName(event.target.value);
+  const handleChangeSelectBusiness = (event) => {
+    setApplication(event.target.value);
+    setCategory('');
+    setGroup('');
+    setProduct('');
+  };
+  const handleChangeSelectCategory = (event) => {
+    setCategory(event.target.value);
+    setGroup('');
+    setProduct('');
+  };
+  const handleChangeSelectGroup = (event) => {
+    setGroup(event.target.value);
+    setProduct('');
+  };
+
+  const handleChangeSelectProduct = (event) => {
+    setProduct(event.target.value);
+  };
+
+  const [thumb, setThumb] = useState('');
+  const canCreate =
+    name && thumb && application && category && grouping && product;
+
+  const [file, setFile] = useState();
+
+  const [uploading, setUploading] = useState(false);
+
+  const {data, getItem} = useGetItem<{url: string; value: string}>(
+    '/products/uploadThumbnailUrl',
+  );
+
+  const {post, loading, data: postData} = usePost('/products');
+
+  useEffect(() => {
+    if (data) {
+      uploadFile(data.url, file)
+        .then(() => setThumb(data.value))
+        .finally(() => setUploading(false));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (postData) {
+      toast({
+        title: 'Successfully!',
+        status: 'success',
+        duration: 2000,
+        position: 'top-right',
+        isClosable: true,
+      });
+    }
+  }, [postData]);
+
+  return (
+    <UI.Box py={5} px={7}>
+      <UI.HStack
+        w="full"
+        _hover={{cursor: 'pointer'}}
+        onClick={() => push('/home/content-management/line-of-business')}>
+        <BsArrowLeft size={20} />
+        <UI.Text fontSize={'14px'}>Back</UI.Text>
+      </UI.HStack>
+      <UI.Text fontSize="24px" fontWeight="bold">
+        Content Management - Products
+      </UI.Text>
+      <UI.VStack
+        spacing="20px"
+        alignItems="flex-start"
+        mt={3}
+        py={5}
+        px={7}
+        bg="white"
+        shadow="md">
+        <UI.Text fontSize="16px" fontWeight="bold">
+          ADD NEW PRODUCT
+        </UI.Text>
+        <UI.HStack w="full">
+          <UI.Text w="300px">Product Name</UI.Text>
+          <UI.Input value={name} onChange={handleChange} />
+        </UI.HStack>
+        <UI.HStack w="full">
+          <UI.Text w="300px">Select Line of Business</UI.Text>
+          <UI.Select
+            placeholder={'Select Line of Business'}
+            onChange={handleChangeSelectBusiness}>
+            {menuData &&
+              menuData.map((x) => {
+                return <option value={x?.id}>{x?.name}</option>;
+              })}
+          </UI.Select>
+        </UI.HStack>
+        <UI.HStack w="full">
+          <UI.Text w="300px">Select Line of Product</UI.Text>
+          <UI.Select
+            placeholder={'Select Line of Product'}
+            onChange={handleChangeSelectCategory}>
+            {categoriesData &&
+              categoriesData?.records.map((x) => {
+                return <option value={x?.id}>{x?.name}</option>;
+              })}
+          </UI.Select>
+        </UI.HStack>
+        <UI.HStack w="full">
+          <UI.Text w="300px">Select Product Group</UI.Text>
+          <UI.Select
+            placeholder={'Select Product Group'}
+            onChange={handleChangeSelectGroup}>
+            {groupingsData &&
+              groupingsData?.records.map((x) => {
+                return <option value={x?.id}>{x?.name}</option>;
+              })}
+          </UI.Select>
+        </UI.HStack>
+        <UI.HStack w="full">
+          <UI.Text w="300px">Select Product</UI.Text>
+          <UI.Select
+            placeholder={'Select Product Group'}
+            onChange={handleChangeSelectProduct}>
+            {productsData &&
+              productsData?.records.map((x) => {
+                return <option value={x?.id}>{x?.name}</option>;
+              })}
+          </UI.Select>
+        </UI.HStack>
+        <UI.HStack alignItems="flex-start" w="full">
+          <UI.Text w="300px">Upload Image</UI.Text>
+          <UI.VStack alignItems="flex-start" w="full">
+            <UploadThumb
+              name="thumb"
+              isLoading={uploading}
+              onChangeValue={({thumb}) => {
+                if (thumb?.[0]) {
+                  setUploading(true);
+                  setFile(thumb?.[0]);
+                  getItem({
+                    name: thumb?.[0]?.name,
+                    type: thumb?.[0]?.type,
+                  });
+                }
+              }}
+            />
+            {thumb && (
+              <UI.Box position="relative">
+                <UI.Circle
+                  onClick={() => {
+                    setThumb('');
+                    setFile(null);
+                  }}
+                  cursor="pointer"
+                  bg="white"
+                  right={-2}
+                  top={-2}
+                  position="absolute">
+                  <IoMdCloseCircle fontSize="20px" color="red" />
+                </UI.Circle>
+                <UI.Image
+                  boxSize="120px"
+                  borderRadius="md"
+                  objectFit="cover"
+                  src={thumb}
+                />
+              </UI.Box>
+            )}
+          </UI.VStack>
+        </UI.HStack>
+        <UI.HStack alignItems="flex-start" w="full">
+          <UI.Text w="300px">Choose Stock Image</UI.Text>
+          <UI.SimpleGrid
+            borderWidth="2px"
+            borderRadius="md"
+            p={5}
+            w="full"
+            gap="20px"
+            templateColumns="repeat(auto-fill, 180px)">
+            {STOCK.map((x, i) => (
+              <UI.Box cursor="pointer" onClick={() => setThumb(x)} key={i}>
+                <UI.Image src={x} />
+              </UI.Box>
+            ))}
+          </UI.SimpleGrid>
+        </UI.HStack>
+        <UI.Center w="full">
+          <UI.Button
+            isLoading={loading}
+            onClick={() =>
+              post({
+                grouping,
+                name,
+                mediaDestination: thumb,
+              })
+            }
+            isDisabled={!canCreate}
+            w="150px">
+            Create
+          </UI.Button>
+        </UI.Center>
+      </UI.VStack>
+    </UI.Box>
+  );
 }
 
 export default AddNew;
--- src/modules/home/modules/content-management/modules/Modules/pages/list.tsx
+++ src/modules/home/modules/content-management/modules/Modules/pages/list.tsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import * as UI from '@chakra-ui/react';
+import ContentView from '@components/ContentView';
+import FormGenerate from '@components/FormGenerate';
+import {useGetList, useFilter, useGetItem} from '@utils/hooks';
+import {IProduct, ICategorie, IGrouping} from '@types';
 
 function List() {
-  return <div>This is list</div>;
+  const {getItem: getAllMenu, data: menuData} = useGetItem('applications/menu');
+  useEffect(() => {
+    getAllMenu();
+  }, []);
+
+  const handleOnChange = ({business, category, grouping}) => {
+    if (business) getListCategories({limit: 9999,filter: JSON.stringify([{application: business}])})
+    if (category) getListGroupings({limit: 9999,filter: JSON.stringify([{category: category}])})
+    getListProduct({filter: grouping ? JSON.stringify([{grouping: grouping}]): undefined})
+
+
+  };
+
+  //const {getList: getListApplications, data: lineOfBusinessData} = useGetList<IApplication>('applications');
+  const {getList: getListCategories, data: categoriesData} = useGetList<ICategorie>('categories');
+  const {getList: getListGroupings, data: groupingsData} = useGetList<IGrouping>('groupings');
+  const {getList: getListProduct, data: productsData} = useGetList<IProduct>('products');
+
+
+  const {page, limit} = useFilter({limit: 10, page: 1});
+  useEffect(() => {
+    getListProduct({
+      page,
+      limit,
+    });
+  }, [page, limit]);
+
+
+  return (
+    <UI.Box minH="89vh">
+      <ContentView
+        data={productsData?.records}
+        limit={limit}
+        totalCount={productsData?.total}
+        currentPage={page}
+        filterBarWidth="full"
+        filterBar={
+          <FormGenerate
+            onChangeValue={handleOnChange}
+            gap="10px"
+            w="60vw"
+            mb={4}
+            fields={[
+              {
+                name: 'business',
+                type: 'select',
+                size: 'md',
+                colSpan: 3,
+                placeholder: 'Line of Business',
+                options: menuData?.map((x) => ({
+                  value: x?.id,
+                  label: x?.name,
+                })),
+              },
+              {
+                name: 'category',
+                type: 'select',
+                size: 'md',
+                colSpan: 3,
+                placeholder: 'Line of Product',
+                options: categoriesData?.records.map((x) => ({
+                  value: x?.id,
+                  label: x?.name,
+                })),
+              },
+              {
+                name: 'grouping',
+                type: 'select',
+                size: 'md',
+                colSpan: 3,
+                placeholder: 'Product Group',
+                options: groupingsData?.records.map((x) => ({
+                  value: x?.id,
+                  label: x?.name,
+                })),
+              },
+              {
+                name: 'product',
+                type: 'select',
+                size: 'md',
+                colSpan: 3,
+                placeholder: 'Products',
+                options: groupingsData?.records.map((x) => ({
+                  value: x?.id,
+                  label: x?.name,
+                })),
+              },
+            ]}
+          />
+        }
+        name="Content Management - Products"
+        linkAddNew="/home/content-management/modules/add-new"
+      />
+    </UI.Box>
+  );
 }
 
 export default List;
