760b3b0198e6d90f73ddb96c14653c1c73102bd9
ndvinh98 <kaiznguyen98@gmail.com>
2021-11-18 09:27:25 +0700

    bug fix

--- src/components/ContentView/index.tsx
+++ src/components/ContentView/index.tsx
@@ -15,7 +15,7 @@ import NormalGridItem from './item/NormalGridItem';
 import ModuleGridItem from './item/ModuleGridItem';
 import NormalListItem from './item/NormalListItem';
 import ModuleListItem from './item/ModuleListItem';
-import {isEmpty, name} from 'lodash';
+import {isEmpty} from 'lodash';
 import {useModalController} from '@modules/modal';
 
 export interface IContentView {
@@ -73,6 +73,11 @@ function ContentView(props: IContentView) {
   React.useEffect(() => {
     if (pathname) clear();
   }, [pathname]);
+
+  React.useEffect(() => {
+    clear();
+  }, [data]);
+
 
   return (
     <UI.VStack
